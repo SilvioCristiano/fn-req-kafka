973cd6e45da242e88091fb591617d7a067d5c436
SilvioCristiano <sivuca1@gmail.com>
2021-04-20 18:52:22 -0300

    first commit

---
+++ src/test/java/com/fn/kafka/EventMsgTest.java
@@ -0,0 +1,14 @@
+package com.fn.kafka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EventMsgTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
